2f500b7c30e51cbe395eec4e78e3b25cdb751374
anitaprova <104943181+anitaprova@users.noreply.github.com>
2022-11-30 20:13:16 -0500

    task b

--- main.cpp
+++ main.cpp
@@ -2,7 +2,19 @@
 #include "recursion.h"
 
 int main() {
+	//TASK A
 	printRange(-2, 10);
-
+	std::cout << "\n";
+	
+	//TASK B
+	int x = sumRange(1, 3);
+	std::cout << "This is " << x << std::endl;   // 6
+	
+	int y = sumRange(-2, 10);
+	std::cout << "That is " << y << std::endl;
+		
+	//TASK C
+	
+	
 	return 0;
 }
--- recursion.cpp
+++ recursion.cpp
@@ -6,3 +6,12 @@ void printRange(int left, int right){
 		printRange(left + 1, right);
 	}
 }
+
+int sumRange(int left, int right) {
+	if(left == right) {
+		return right;
+	}
+	else {
+		return left + sumRange(left + 1, right);
+	}
+}
